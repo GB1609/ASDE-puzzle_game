2aa8568aa9b0a9ff2deaf70009ebe4819c203e44
pie93 <pie93@192.168.0.101>
2018-11-16 16:02:00 +0100

    Added UserController class Added CredentialsDAO class Added MatchDAO class Added AccountService class Added Credentials class Added Match class Added User class
    Added Index page for testing
    Added dependency on Pom.xml
    
    Fixed DispatcherConfiguration settings
    Implemented Login functionality
    Implemented Logout functionality
    Implemented Sign up functionality
    Implemented Match persistence functionality
    Implemented Credential persistence functionality

---
+++ src/it/mat/unical/asde/project2_puzzle/components/service/AccountService.java
@@ -0,0 +1,39 @@
+package it.mat.unical.asde.project2_puzzle.components.service;
+
+import java.util.ArrayList;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.mat.unical.asde.project2_puzzle.components.persistence.MatchDAO;
+import it.mat.unical.asde.project2_puzzle.components.persistence.CredentialsDAO;
+import it.mat.unical.asde.project2_puzzle.model.Credentials;
+import it.mat.unical.asde.project2_puzzle.model.Match;
+
+@Service
+public class AccountService {
+
+	@Autowired
+	private MatchDAO matchDAO;
+
+	@Autowired
+	private CredentialsDAO userDAO;
+
+
+	public boolean loginAccepted(String username, String password) {
+		return userDAO.exists(new Credentials(username, password));
+	}
+
+	public boolean accountCreated(String username, String password) {
+		return userDAO.save(new Credentials(username, password));
+	}
+
+	public ArrayList<Match> getMatches(String username) {
+		return matchDAO.getMatches(username);
+	}
+
+	public void addMatch(Match match) {
+		matchDAO.save(match);
+	}
+}
--- src/it/mat/unical/asde/project2_puzzle/configuration/DispatcherConfiguration.java
+++ src/it/mat/unical/asde/project2_puzzle/configuration/DispatcherConfiguration.java
@@ -42,7 +42,6 @@ public class DispatcherConfiguration implements WebMvcConfigurer{
         try {
             lsfb.afterPropertiesSet();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         return lsfb.getObject();
