152d4af0d58fe4e5182ab45dcc467e353833c326
gb1609 <gb1609@outlook.com>
2018-11-18 18:13:45 +0100

    management 2 grid

--- src/it/mat/unical/asde/project2_puzzle/components/controllers/GameController.java
+++ src/it/mat/unical/asde/project2_puzzle/components/controllers/GameController.java
@@ -7,29 +7,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import it.mat.unical.asde.project2_puzzle.components.services.GameService;
 
 @Controller
-public class GameController {
-	@Autowired
-	GameService gameService;
+public class GameController{
+    @Autowired
+    GameService gameService;
 
-	@GetMapping("/")
-	public String goToGame(Model m) {
-		m.addAttribute("randomGrid", gameService.getRandomGrid());
-		return "Game";
-	}
+    @GetMapping("/")
+    public String goToGame(Model m){
+        m.addAttribute("randomGrid", gameService.getRandomGrid());
+        return "Game";
+    }
 
-	@PostMapping("move_piece") // le richieste ajax solitamente sono in get
-	@ResponseBody // per dire che ciò che restituisce non è il nome della view ma il risultato
-					// della richiesta ajax
-	public void movePiece(@RequestParam String new_location, @RequestParam String old_location,
-			@RequestParam String piece, @RequestParam int new_position) {
-		System.out.println("nl: " + new_location);
-		System.out.println("ol: " + old_location);
-		System.out.println("piece: " + piece);
-		System.out.println("np:" + new_position);
-
-	}
+    @PostMapping("/move_piece") // le richieste ajax solitamente sono in get
+    @ResponseBody // per dire che ciò che restituisce non è il nome della view ma il risultato
+                  // della richiesta ajax
+    public void movePiece(@RequestParam String new_location,@RequestParam String old_location,@RequestParam String piece,
+            @RequestParam int new_position){
+        if (new_location.equals(old_location) && new_location.equals("to_complete")) {
+            gameService.shuffle_matrix_toComplete(piece, new_position);
+        } else if (new_location.equals("to_complete") && !new_location.equals(old_location))
+            gameService.insert_in_matrix_toComplete(piece, new_position);
+//        gameService.printToComplete();
+        if (gameService.check_completation())
+            System.out.println("WIN");
+    }
 }
