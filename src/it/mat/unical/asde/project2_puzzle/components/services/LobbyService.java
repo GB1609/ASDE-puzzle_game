92f1960fb306f78ab287ead0d7d0b3ddd7354435
amato_davide <amatodavide1@gmail.com>
2018-11-20 16:12:21 +0100

    Fixed infinitely many bugs (semicit.)
    Most important things:
    -fixed Lobby.js functions
    -fixed lobby search by name
    -added lobby search bu username
    -fixed jump from lobby view to game view

--- src/it/mat/unical/asde/project2_puzzle/components/services/LobbyService.java
+++ src/it/mat/unical/asde/project2_puzzle/components/services/LobbyService.java
@@ -1,10 +1,8 @@
 package it.mat.unical.asde.project2_puzzle.components.services;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 import javax.annotation.PostConstruct;
 
@@ -16,46 +14,96 @@ import it.mat.unical.asde.project2_puzzle.model.Lobby;
 public class LobbyService {
 
 	List<Lobby> lobbies;
-	
+
 	@PostConstruct
 	public void init() {
-		lobbies = new LinkedList<Lobby>();
-		for(int i = 0; i <= 5; i++) {
-			lobbies.add(new Lobby("Lobby"+(i+1),"user "+(int)(Math.random()*100),""));
+		this.lobbies = new LinkedList<>();
+		for (int i = 0; i <= 5; i++) {
+			this.lobbies.add(new Lobby("Lobby" + (i + 1), "user" + (int) (Math.random() * 100), ""));
 		}
 	}
 
 	public List<Lobby> getLobbies() {
-		return lobbies;
+		return this.lobbies;
 	}
-	
+
 	public boolean addLobby(Lobby newLobby) {
-		//TODO can't add lobby with name with spaces
+		// TODO can't add lobby with name with spaces
 		boolean added = false;
-		if(!lobbies.contains(newLobby)) {
-			lobbies.add(0, newLobby);
+		if (!this.lobbies.contains(newLobby)) {
+			this.lobbies.add(0, newLobby);
 			added = true;
 		}
 		return added;
 	}
 
-	//Return the lobby with the given name, null otherwise
+	// Return the lobby with the given name, null otherwise
 	public Lobby getLobbyByName(String name) {
-		for (int i = 0; i < lobbies.size(); i++) {
-			Lobby lobby = lobbies.get(i);
-			if(lobby.getName().equals(name)){
-				putLobbyOnTop(i);
+		for (int i = 0; i < this.lobbies.size(); i++) {
+			Lobby lobby = this.lobbies.get(i);
+			if (lobby.getName().equals(name)) {
+				this.putLobbyOnTop(i);
 				return lobby;
 			}
 		}
 		return null;
 	}
-	
+
 	public void putLobbyOnTop(int index) {
-		Collections.rotate(lobbies.subList(0, index+1), 1);
+		Collections.rotate(this.lobbies.subList(0, index + 1), 1);
 	}
-	
+
 	public boolean removeLobby(Lobby lobby) {
-		return lobbies.remove(lobby);
+		return this.lobbies.remove(lobby);
+	}
+
+	public boolean removeLobbyByName(String lobby_name) {
+		for (Lobby lobby : this.lobbies) {
+			if (lobby.getName().equals(lobby_name)) {
+				return this.lobbies.remove(lobby);
+			}
+		}
+		return false;
+	}
+
+	public Lobby getLobbyByUsername(String username) {
+		for (int i = 0; i < this.lobbies.size(); i++) {
+			Lobby lobby = this.lobbies.get(i);
+			if (lobby.getOwner().equals(username)) {
+				this.putLobbyOnTop(i);
+				return lobby;
+			}
+		}
+		return null;
+	}
+
+	// user with "username" leave the lobby where it is
+	public void leaveIfInOtherLobby(String username) {
+		System.out.println("LEAVE if in other lobby");
+		for (int i = 0; i < this.lobbies.size(); i++) {
+			Lobby lobby = this.lobbies.get(i);
+			String guest = lobby.getGuest();
+			if (lobby.getOwner().equals(username)) {
+				if (guest != null) {
+					lobby.setOwner(guest);
+					lobby.setGuest(null);
+					System.out.println("LEAVE LOBBY: " + lobby);
+				} else {
+					this.lobbies.remove(i);
+					System.out.println("REMOVE LOBBY: " + lobby);
+				}
+			} else if (guest != null) {
+				if (guest.equals(username)) {
+					System.out.println("LEAVE LOBBY: " + lobby);
+					lobby.setGuest(null);
+				}
+			}
+		}
+	}
+
+	public void joinToLobby(String lobby_name, String username) {
+		this.leaveIfInOtherLobby(username);
+		Lobby lobbyToJoin = this.getLobbyByName(lobby_name);
+		lobbyToJoin.setGuest(username);
 	}
 }
--- src/it/mat/unical/asde/project2_puzzle/model/Lobby.java
+++ src/it/mat/unical/asde/project2_puzzle/model/Lobby.java
@@ -3,56 +3,66 @@ package it.mat.unical.asde.project2_puzzle.model;
 public class Lobby {
 
 	private String name;
-	private String user1;
-	private String user2;
-	public Lobby(String name, String user1, String user2) {
-		super();
-		this.name = name;
-		this.user1 = user1;
-		this.user2 = user2;
-	}
-	public Lobby(String name, String user1) {
-		super();
-		this.name = name;
-		this.user1 = user1;
-	}
+	private String owner;
+	private String guest;
+
 	public String getName() {
-		return name;
+		return this.name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getUser1() {
-		return user1;
+
+	public Lobby(String name) {
+		super();
+		this.name = name;
 	}
-	public void setUser1(String user1) {
-		this.user1 = user1;
+
+	public Lobby(String name, String owner) {
+		super();
+		this.name = name;
+		this.owner = owner;
 	}
-	public String getUser2() {
-		return user2;
+
+	public Lobby(String name, String owner, String guest) {
+		super();
+		this.name = name;
+		this.owner = owner;
+		this.guest = guest;
 	}
-	public void setUser2(String user2) {
-		this.user2 = user2;
+
+	public String getOwner() {
+		return this.owner;
 	}
-	
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getGuest() {
+		return this.guest;
+	}
+
+	public void setGuest(String guest) {
+		this.guest = guest;
+	}
+
 	@Override
 	public String toString() {
-		return "Lobby [name=" + name + ", user1=" + user1 + ", user2=" + user2 + "]";
+		return "Lobby [name=" + this.name + ", owner=" + this.owner + ", guest=" + this.guest + "]";
 	}
-	
-	
+
 	@Override
 	public boolean equals(Object obj) {
-		//Two lobbies are equals if they have the same "name"
-		//In this way a user can search lobbies by "name"
-		return name.equals(((Lobby)obj).getName());
+		// Two lobbies are equals if they have the same "name"
+		// In this way a user can search lobbies by "name"
+		return this.name.equals(((Lobby) obj).getName());
 	}
-	
+
 	@Override
 	public int hashCode() {
-		return name.hashCode();
+		return this.name.hashCode();
 	}
-	
-	
-	
+
 }
